16748ab4e3710ac8f73fb8a0aab2ac7640ef12f2
franklin rodriguez <franklinserif@gmail.com>
2022-05-03 22:52:28 -0400

    Added more styles for App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ const Container = styled.div`
   max-width: 900px;
   margin: 0 auto;
   width: 90%;
+
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
 `;
 
 const Imagen = styled.img`
@@ -17,13 +23,27 @@ const Imagen = styled.img`
 const Heading = styled.h1`
   font-family: 'Lato', sans-serif;
   color: white;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &:after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
 `;
 
 function App() {
   return (
     <Container>
-      <Imagen src={ImageCripto} alt="criptos image" />
-      <Heading>App</Heading>
+      <Imagen src={ImageCripto} alt="criptos imagen" />
+      <Heading>Cotiza criptomonedas al instante</Heading>
     </Container>
   );
 }
