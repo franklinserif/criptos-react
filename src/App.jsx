1ad85666eecae3675e6c739bcd6c5bda7d1cca90
franklin rodriguez <franklinserif@gmail.com>
2022-05-03 22:46:10 -0400

    Added styles with emotion and image to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import styled from '@emotion/styled';
+import ImageCripto from './assets/img/imagen-criptos.png';
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+`;
+
+const Imagen = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`;
+
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: white;
+`;
+
 function App() {
-  return <div>App</div>;
+  return (
+    <Container>
+      <Imagen src={ImageCripto} alt="criptos image" />
+      <Heading>App</Heading>
+    </Container>
+  );
 }
 
 export default App;
